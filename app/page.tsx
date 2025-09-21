324132b4f4a2953d8c5516fafa64337ab41dd216
Abdulwaheed Qamardeen <4621qamardeen@example.com>
2025-09-21 18:42:12 +0100

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-blue-500">
+      <h1 className="text-white text-4xl font-bold">
+        Hello, Tailwind v4 + Next.js!
+      </h1>
+    </div>
+  );
+}
